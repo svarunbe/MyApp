cfaa578639edd6725e917b09484f7d794288789c
vijay joshi <viayjoshi@gmail.com>
2017-09-25 17:11:32 +0400

    code refactored

--- server/strategies/strategyCronJob.js
+++ server/strategies/strategyCronJob.js
@@ -6,7 +6,8 @@ var MACD = require('technicalindicators').MACD;
 var EMA = require('technicalindicators').EMA;
 var ATR = require('technicalindicators').ATR;
 var ADX = require('technicalindicators').ADX;
-let KST = require('technicalindicators').KST
+var KST = require('technicalindicators').KST;
+var RSI = require('technicalindicators').RSI;
 var socketPort = require('../modules/socketConnection.js');
 let mongo = require('../modules/mongoConnection.js');
 var KiteTicker = require("kiteconnect").KiteTicker;
@@ -33,33 +34,33 @@ let kiteLogin = function(session_token, job, fn) {
 }
 let generateData = function(resData, closing, strategy, callback) {
     var resultArrayPattern = {
-        'high':[],
-        'low':[],
-        'close':[]
+        'high': [],
+        'low': [],
+        'close': []
     };
-    var resultObjectPattern=[];
+    var resultObjectPattern = [];
     resData.filter(function(val) {
         resultArrayPattern.close.push(val[closing]);
         resultArrayPattern.high.push(val[2]);
         resultArrayPattern.low.push(val[3]);
         resultObjectPattern.push({
-            'date':val[0],
-            'open':val[1],
-            'high':val[2],
-            'low':val[3],
-            'close':val[4],
-            'volume':val[5]
+            'date': val[0],
+            'open': val[1],
+            'high': val[2],
+            'low': val[3],
+            'close': val[4],
+            'volume': val[5]
         })
         return;
     });
-    callback(resultArrayPattern,resultObjectPattern,strategy);
+    callback(resultArrayPattern, resultObjectPattern, strategy);
 }
-let averageTrueRange=function(period,high,low,close){
+let averageTrueRange = function(period, high, low, close) {
     var input = {
-      high : high,
-      low  : low,
-      close : close,
-      period : period
+        high: high,
+        low: low,
+        close: close,
+        period: period
     }
     return ATR.calculate(input);
 }
@@ -68,38 +69,46 @@ let getMACD = function(data, fastPeriod, slowPeriod) {
         values: data,
         fastPeriod: fastPeriod,
         slowPeriod: slowPeriod,
-        signalPeriod: 3,
+        signalPeriod: 7,
         SimpleMAOscillator: false,
         SimpleMASignal: false
     }
     return MACD.calculate(macdInput);
 }
-let getADX = function(period,high,low,close){
+let getADX = function(period, high, low, close) {
     var input = {
-      high : high,
-      low  : low,
-      close : close,
-      period : period
+        high: high,
+        low: low,
+        close: close,
+        period: period
     }
     return ADX.calculate(input);
 }
-let getKST = function(period,high,low,close){
+let getRSI = function(period, close) {
+    var inputRSI = {
+        values: close,
+        period: period
+    };
+    return RSI.calculate(inputRSI)
+}
+
+let getKST = function(period, high, low, close) {
     var input = {
-      values      : close,
-      ROCPer1     : 10,
-      ROCPer2     : 15,
-      ROCPer3     : 20,
-      ROCPer4     : 30,
-      SMAROCPer1  : 10,
-      SMAROCPer2  : 10,
-      SMAROCPer3  : 10,
-      SMAROCPer4  : 15,
-      signalPeriod: 3
+        values: close,
+        ROCPer1: 5,
+        ROCPer2: 10,
+        ROCPer3: 15,
+        ROCPer4: 20,
+        SMAROCPer1: 5,
+        SMAROCPer2: 10,
+        SMAROCPer3: 15,
+        SMAROCPer4: 20,
+        signalPeriod: 9
     };
     return KST.calculate(input)
 }
-let getEMA=function(data,period){
-    return EMA.calculate({period : period, values : data});     
+let getEMA = function(data, period) {
+    return EMA.calculate({ period: period, values: data });
 }
 
 let getShares = function(job, fn) {
@@ -108,148 +117,89 @@ let getShares = function(job, fn) {
         db.collection('shares').find().toArray(function(err, result) {
             if (err) throw err;
             _global.shares = result;
-            kiteControl.historical(result[0].token, "2017-08-25", "2017-09-20", "15minute")
+            var date = new Date();
+
+            var todayDate = date.getFullYear() + '-' +
+                ((date.getMonth() + 1).toString().length == 1 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)) +
+                '-' + date.getDate();
+            var lastDate = new Date(date.setDate(date.getDate() - 29));
+
+            lastDate = lastDate.getFullYear() + '-' +
+                ((lastDate.getMonth() + 1).toString().length == 1 ? '0' + (lastDate.getMonth() + 1) : (lastDate.getMonth() + 1)) +
+                '-' + lastDate.getDate();
+
+            kiteControl.historical(result[2].token, lastDate, todayDate, "15minute")
                 .then(function(response) {
-                    //console.log(response);
-                    var len = response.data.candles.length;
-                    //var responceWithTR=fn.calculateTrueRange(response.data.candles);
-                    //console.log(response.data.candles[len - 1]);
-                    fn.generateData(response.data.candles, 4, _global.shares[0].strategy, function(resArr,resObj,strategy) {
-                        
-
-
+                    fn.generateData(response.data.candles, 4, _global.shares[0].strategy, function(resArr, resObj, strategy) {
                         if (strategy == "macd") {
-                            var macdOutput = fn.getMACD(resArr.close,5,8);
-                            var ema_2_days=fn.getEMA(resArr.close,2);
-                            var ema_5_days=fn.getEMA(resArr.close,5);
-                            var avgTrueRange=fn.averageTrueRange(7,resArr.high,resArr.low,resArr.close);
-                            var arr_adx=fn.getADX(7,resArr.high,resArr.low,resArr.close);
-                            var kstArr=fn.getKST(7,resArr.high,resArr.low,resArr.close);
-                            //console.log(kstArr);
-                            var structuredData=fn.createStructure(response.data.candles,macdOutput,ema_2_days,ema_5_days,avgTrueRange,arr_adx,kstArr);                            
-                            var flagMacd = "";
-                            var flagEma="";
-                            var profitADX = 0;
-                            var profitEma = 0;
-                            var profitKST=0;
-                            var profitMacd=0;
-                            //console.log(structuredData);
-                            var trendADX="";
-                            var trendEMA="";
-                            var trendKST="";
-                            var trendmacd="";
+                                var profitMacd = 0,
+                                trendmacd = "";
 
-                            for (var i = 0; i < structuredData.length; i++) {
-                                var d = new Date(structuredData[i].date);
-                                var today = new Date("2017-09-20T09:15:00+0530");
-                                if (d.getDate() === today.getDate()) { 
-                                    //ADX Strategy
-   /*if(structuredData[i].pdm > structuredData[i].mdm && trendADX!="up"  && structuredData[i].adx > 20){
-    console.log("uptrend ADX"+structuredData[i].date)
-    trendADX="up";
-    profitADX -= structuredData[i].close;
-   }else if(trendADX=="up" && ((structuredData[i].adx-4) < structuredData[i-1].adx)) {  // (structuredData[i].pdm < structuredData[i].mdm && trend!="down"){
-    console.log("downtrend ADX"+structuredData[i].date)
-    trendADX="down";
-    profitADX += structuredData[i].close;
-   }
+                                if (trendmacd != "up" && structuredData.adx > 20 && structuredData.rsi < 30 && structuredData.kst > structuredData.kstSignal && structuredData.macd > 0 && structuredData.macd > structuredData.signal) {
+                                    console.log("buy " + structuredData.date);
+                                    trendmacd = "up";
+                                    profitMacd -= structuredData.close;
+                                } else if (trendmacd == "up" && structuredData.kst < structuredData.kstSignal) {
+                                    console.log("sell " + structuredData.date);
+                                    trendmacd = "down";
+                                    profitMacd += structuredData.close;
+                                }
+                                /*buy signal*/
+                                // adx > 30
+                                // macd > signal && macd > 0
+                                // kst > 0 && kast >  kstSignal
+                                // rsi < 30
 
-   //EMA strategy
-    if ((structuredData[i].ema_2_days > structuredData[i-1].ema_2_days) && (structuredData[i].ema_2_days > structuredData[i].ema_5_days) && trendEMA!="up" && (structuredData[i].adx > structuredData[i-1].adx) && (structuredData[i].pdm > structuredData[i].mdm)) {
-        console.log("uptrend EMA"+structuredData[i].date+ " "+structuredData[i].close)
-        trendEMA = "up";
-        profitEma -= structuredData[i].close;
-    } else if ((structuredData[i].ema_2_days < structuredData[i-1].ema_2_days) && (structuredData[i].ema_2_days < structuredData[i].ema_5_days || structuredData[i].pdm < (structuredData[i].adx-5) ) && trendEMA=="up") {
-        console.log("downtrend EMA"+structuredData[i].date+" "+structuredData[i].close)
-        trendEMA = "down";
-        profitEma += structuredData[i].close;
-    }
-   //KST strategy
-   if(structuredData[i].kstSignal!=undefined && structuredData[i].kst > structuredData[i].kstSignal && structuredData[i].kst > 0 && trendKST!="up") {
-        console.log("uptrend KST"+structuredData[i].date+ " "+structuredData[i].close)
-        trendKST = "up";
-        profitKST -= structuredData[i].close;
-   }else if(structuredData[i].kstSignal!=undefined && structuredData[i].kst < structuredData[i].kstSignal && trendKST=="up"){
-        console.log("downtrend KST"+structuredData[i].date+" "+structuredData[i].close)
-        trendKST = "down";
-        profitKST += structuredData[i].close;
-   }
-
-   //macd strategy
-   if(structuredData[i].macd > 0 && structuredData[i].macd > structuredData[i].signal && trendmacd!="up") {
-        console.log("uptrend macd"+structuredData[i].date+ " "+structuredData[i].close)
-        trendmacd = "up";
-        profitMacd -= structuredData[i].close;
-   }else if(structuredData[i].macd < structuredData[i].signal && trendmacd=="up"){
-        console.log("downtrend macd"+structuredData[i].date+" "+structuredData[i].close)
-        trendmacd = "down";
-        profitMacd += structuredData[i].close;
-   }*/
-
-   if( trendmacd!="up" && structuredData[i].adx > 20 && structuredData[i].kst > structuredData[i].kstSignal && structuredData[i].macd > 0 && structuredData[i].macd > structuredData[i].signal){
-        console.log("buy "+structuredData[i].date);
-        trendmacd="up";
-        profitMacd -= structuredData[i].close;
-   }else if(trendmacd=="up" && structuredData[i].kst < structuredData[i].kstSignal){
-        console.log("sell "+structuredData[i].date);
-        trendmacd="down";
-        profitMacd += structuredData[i].close;
-   }
-   // adx > 30
-   // macd > signal && macd > 0
-   // kst > 0 && kast >  kstSignal
-   //
-
-
-
-}
-}console.log(profitMacd);
-//console.log("total profit macd" + profitMacd  + " total profit Ema " + profitEma + " total profit KST " + profitKST +" total profit ADX " + profitADX);
+                                /*sell signal signal*/
+                                // kast <  kstSignal
+                                console.log(profitMacd);
                         }
                     });
                 })
                 .catch(function(err) {
                     console.log(err);
                 });
-            //result[0].
             //job.start();
             db.close();
         });
     });
 }
-let createStructure=function(data,macd,ema_2_days,ema_5_days,atr,adx,kst){
-    data=data.reverse();
-    macd=macd.reverse();
-    ema_2_days=ema_2_days.reverse();
-    ema_5_days=ema_5_days.reverse();
-    adx=adx.reverse();
-    var newData=[];
-    for(var i=0;i<data.length;i++){
-        if(macd[i] && ema_2_days[i] && ema_5_days[i] && atr[i] && adx[i] && kst[i]){
-            var currentPrice=(data[i][1]+data[i][2]+data[i][3]+data[i][4])/4;
-            var Previous_Close=data[i]
-            newData.push({
-            'date':data[i][0],
-            'open':data[i][1],
-            'high':data[i][2],
-            'low':data[i][3],
-            'close':data[i][4],
-            'volume':data[i][5],
-            'macd':macd[i].MACD,
-            'signal':macd[i].signal,
-            'ema_2_days':ema_2_days[i],
-            'ema_5_days':ema_5_days[i],
-            'adx':adx[i].adx,
-            'pdm':adx[i].pdi,
-            'mdm':adx[i].mdi,
-            'kst':kst[i].kst,
-            'kstSignal':kst[i].signal
-         });    
-        }else{
-            i=data.length;
-        }    
+let createStructure = function(data,) {
+    var macdOutput = fn.getMACD(resArr.close, 5, 10),
+        ema_2_days = fn.getEMA(resArr.close, 2),
+        ema_5_days = fn.getEMA(resArr.close, 5),
+        arr_rsi = fn.getRSI(7, resArr.close),
+        avgTrueRange = fn.averageTrueRange(7, resArr.high, resArr.low, resArr.close),
+        arr_adx = fn.getADX(7, resArr.high, resArr.low, resArr.close),
+        kstArr = fn.getKST(7, resArr.high, resArr.low, resArr.close),
+        structuredData = fn.createStructure(response.data.candles, macdOutput, ema_2_days, ema_5_days, avgTrueRange, arr_adx, kstArr, arr_rsi),
+    
+
+    data = data.reverse()[0];
+    macd = macd.reverse()[0];
+    ema_2_days = ema_2_days.reverse()[0];
+    ema_5_days = ema_5_days.reverse()[0];
+    adx = adx.reverse()[0];
+    rsi = rsi.reverse()[0];
+
+    return {
+        'date': data[0],
+        'open': data[1],
+        'high': data[2],
+        'low': data[3],
+        'close': data[4],
+        'volume': data[5],
+        'macd': macd.MACD,
+        'signal': macd.signal,
+        'ema_2_days': ema_2_days,
+        'ema_5_days': ema_5_days,
+        'adx': adx.adx,
+        'pdm': adx.pdi,
+        'mdm': adx.mdi,
+        'kst': kst.kst,
+        'kstSignal': kst.signal,
+        'rsi': rsi
     }
-    return newData.reverse();
 }
 let simpleMovingAvg = function(marketdata, getFn) {
     var prices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 15];
@@ -276,15 +226,16 @@ let checkKiteAccessToken = function(access_token) {
 module.exports = {
     'simpleMovingAvg': simpleMovingAvg,
     'getMACD': getMACD,
-    'getEMA':getEMA,
+    'getEMA': getEMA,
     'generateData': generateData,
     'kiteLogin': kiteLogin,
     //'getHistoricalData':getHistoricalData,
     'getShares': getShares,
     'checkKiteAccessToken': checkKiteAccessToken,
-    'createStructure':createStructure,
-    'averageTrueRange':averageTrueRange,
-    'getADX':getADX,
-    'getKST':getKST
+    'createStructure': createStructure,
+    'averageTrueRange': averageTrueRange,
+    'getADX': getADX,
+    'getKST': getKST,
+    'getRSI': getRSI
     //'getAccessToken':getAccessToken
 };
